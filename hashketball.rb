c7a3d11aeda1d0d68301ff5dd20d1258cb907dfc
Brian Wong <bwong92@gmail.com>
2019-08-02 02:23:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -185,20 +185,20 @@ def big_shoe_rebounds
   player_name = ""
   shoe_size = 0
   game_hash.each do |teams, infos|
-    infos.each do |attribute, info|
-      if info.class == Hash
-        info.each do |player, stats|
-            stats.each do |stat, int|
-              if stat == :shoe
-                if int > shoe_size
-                  shoe_size = int
-                  player_name = player
+        infos.each do |attribute, info|
+          if info.class == Hash
+            info.each do |player, stats|
+                stats.each do |stat, int|
+                  if stat == :shoe
+                    if int > shoe_size
+                      shoe_size = int
+                      player_name = player
+                    end
+                  end
                 end
               end
-            end
+            return hash[teams][infos][player_name][:rebounds]
           end
-        return hash[location][attribute][player_name][:rebounds]
-      end
-    end
+        end
   end
 end
