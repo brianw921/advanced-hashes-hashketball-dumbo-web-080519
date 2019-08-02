e643e6b8012fdf89602bc61a8a307ae59af99b94
Brian Wong <bwong92@gmail.com>
2019-08-02 01:55:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -173,7 +173,7 @@ end
 def player_stats(name)
   player_stats = {}
   game_hash.each do |teams, info|
-   return info[:players].each do |player_name, player_data|
+   info[:players].each do |player_name, player_data|
      binding.pry
      if player_name == name
        binding.pry
