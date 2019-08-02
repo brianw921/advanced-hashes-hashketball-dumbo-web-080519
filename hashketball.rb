4f8fc50edaa41432c51410c2e6abdedfa9adce9d
Brian Wong <bwong92@gmail.com>
2019-08-02 02:06:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -188,7 +188,7 @@ def big_shoe_rebounds(name)
   game_hash.each do |teams, info| #teams = home,away , info = inside hash
     info[:players].each do |player,attributes|
       if name == player
-        points = attributes[:rebounds]
+        rebound = attributes[:rebounds]
       end
     end
   end
